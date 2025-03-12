43de0dc4dc1d5ac9a5fe6416d187de9b809ed4ec
PCK <charles.kantane@outlook.fr>
2025-03-12 12:37:44 +0100

    feat: background for the main content container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../components/header/header.component';
+import { MainContentBackgroundComponent } from '../components/main-content-background/main-content-background.component';
 import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, MainContentBackgroundComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
