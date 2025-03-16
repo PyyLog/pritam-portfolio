bfff9a8dc92dc408dc53954655824b1912ad7c3c
PCK <charles.kantane@outlook.fr>
2025-03-16 22:43:45 +0100

    feat: about me page design done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../components/header/header.component';
 import { MainContentBackgroundComponent } from '../components/main-content-background/main-content-background.component';
 import { HomepageContentComponent } from '../components/homepage-content/homepage-content.component';
+import { AboutMePageComponent } from '../components/about-me-page/about-me-page.component';
 import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, MainContentBackgroundComponent, HomepageContentComponent, FooterComponent],
+  imports: [HeaderComponent, MainContentBackgroundComponent, HomepageContentComponent, AboutMePageComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
