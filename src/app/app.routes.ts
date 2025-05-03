20c2f45fede91de41de1e23ef5e0ec609038beca
PCK <charles.kantane@outlook.fr>
2025-05-03 16:57:30 +0200

    chore: gh-pages setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { AboutMeComponent } from '../pages/about-me/about-me.component';
 import { TimelineComponent } from '../pages/timeline/timeline.component';
 
 export const routes: Routes = [
-  { path: '', component: HomepageComponent },
+  { path: '', component: HomepageComponent, pathMatch: 'full' },
   { path: 'about-me', component: AboutMeComponent },
   { path: 'timeline', component: TimelineComponent },
   { path: '**', redirectTo: '' },
